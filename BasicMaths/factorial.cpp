677b835039deb963ac00c631516312c8689be526
shailesh karve <shaileshkarve705@gmail.com>
2026-09-22 22:02:48 +0530

    Implement factorial method in Solution class

---
+++ BasicMaths/factorial.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long factorial(int n) {
+        long long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+};
